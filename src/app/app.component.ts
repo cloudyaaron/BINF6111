51cf2d7f97c3987a4a6cc8b11f3a95b62a945dcd
kavithakrishs <kavi.krishna28@gmail.com>
2020-06-16 15:01:13 +1000

    Basic patient info extracted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, VERSION } from '@angular/core';
 import data from './phenotips_2020-06-09_18-16_with_external_id.json';
+import { HashTable } from './classes/hashtable';
 
 @Component({
   selector: 'my-app',
@@ -8,6 +9,15 @@ import data from './phenotips_2020-06-09_18-16_with_external_id.json';
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
-
+  hpoTerms: HashTable<string, any>;
   hpoList = data[0];
+  patients = data;
+  
+  //hpoList = this.hpoTerms;
+
+  constructor() {
+    this.hpoTerms = new HashTable<string, any>();
+    this.hpoTerms.put('HP:0000118', 'Phenotypic abnormality');
+  }
+  terms = this.hpoTerms
 }
