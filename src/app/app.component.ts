9c9561572f79c9060c93b4b45c7fa08302fa7b87
cloudyaaron <yzqaaron@gmail.com>
2020-06-18 13:57:54 +1000

    Search function implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,22 @@ import { HashTable } from './classes/hashtable';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  name = 'Angular ' ;
   hpoTerms: HashTable<string, any>;
   hpoList = data[0];
   patients = data;
-  
+  values = ''
+  patientsLenth = Object.keys(this.patients).length
+
+  onKeyUp(event:any){
+    
+    for(let i=0; i<this.patientsLenth;i++){
+      if(this.patients[i]['report_id'] == event.target.value){
+        this.values=this.patients[i]['sex']
+        break
+      }else{
+        this.values=event.target.value
+      }
+    }
+  }
 }
