b3b029d1c6ae6eb24244e29580f9957e884f0128
cloudyaaron <yzqaaron@gmail.com>
2020-06-19 15:33:38 +1000

    single HPO terms searching inplemented

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { NgBusyModule } from 'ng-busy';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatTree, MatTreeModule } from '@angular/material/tree';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule,NgBusyModule ,MatProgressSpinnerModule, MatTreeModule],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
