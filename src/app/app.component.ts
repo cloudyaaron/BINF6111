0cfc54d846d255b69a0684a8bbae303ae8a1b777
Nick-Bosworth <nicholasbosworth014@gmail.com>
2020-07-06 19:25:13 +1000

    initial sunburst work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,6 +23,7 @@ export class AppComponent  {
   values = ''
   suggest_text=''
   search_result = [];
+  chart_data = [];
   patientsLenth = Object.keys(this.patients).length
   suggested_queries = [];
   showConfig = true;
@@ -159,6 +160,7 @@ export class AppComponent  {
       }
     }
   }
+
   search(search_term: string):any{
     console.log('search')
     var index = 0
@@ -234,6 +236,26 @@ export class AppComponent  {
       }
     }
     return true;
+  }
+
+  // Sunburst graph code
+  chartOption: EChartOption = {
+     visualMap: [{
+        type: 'continuous',
+        min: 0,
+        max: this.getResultNum(),
+        inRange: {
+            color: ['#2D5F73', '#538EA6', '#F2D1B3', '#F2B8A2', '#F28C8C']
+        }
+    }],
+    series: [{
+        type: 'sunburst',
+        data: this.chart_data,
+        radius: [0, '90%'],
+        label: {
+            rotate: 'radial'
+        }
+    }]
   }
 
 
