961e6353e53afa7a7cb3460f238c76115b5fc47d
cloudyaaron <yzqaaron@gmail.com>
2020-08-02 17:45:11 +1000

    2 files modified,bugfix

--- src/app/PatientsList/patients.component.ts
+++ src/app/PatientsList/patients.component.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 //import data from '../phenotips_2020-06-09_18-16_with_external_id.json';
 //import { Patient } from './patient';
 import { initializePatients } from './patientList';
+import {MatDialogModule} from '@angular/material/dialog';
 
 @Component({
   selector: 'patient-list',
